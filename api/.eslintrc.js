fc251d2214e25eb9259d326851c455655f915ebe
Ben Brehaut <bencbrehaut@gmail.com>
2023-01-17 19:12:33 +0000

    adding eslint

---
+++ api/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['@sky-uk/eslint-config-sky-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
--- api/src/server.ts
+++ api/src/server.ts
@@ -6,29 +6,31 @@ import Router from '@koa/router';
 
 const app = new Koa();
 const router = new Router();
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser());
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.use(logger());
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
-router.get('/ping', ctx => {
+router.get('/ping', (ctx) => {
   ctx.body = 'pong';
 });
 
 const server = app
   .listen(PORT, async () => {
+    // eslint-disable-next-line no-console
     console.log(`Server started on ${PORT}`);
   })
-  .on("error", async (error: Error) => {
+  .on('error', async (error: Error) => {
+    // eslint-disable-next-line no-console
     console.log(`There was an error: ${error}`);
   });
 
-export { server }
+export { server };
